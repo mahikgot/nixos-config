2e0b57f73d2d8c43400f77701b06e5f6b9a43a10
Mark Guiang <mahikgot@gmail.com>
2025-12-15 00:08:11 +0800

    init commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A template that shows all standard flake outputs";
+
+  inputs.pkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  # Work-in-progress: refer to parent/sibling flakes in the same repository
+  outputs = all@{ self, pkgs, ... }: {
+    nixosConfigurations.nixos = pkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+} 
