2e3375f576bde485ed63c4fed331e66b8d2a64c7
Mark Guiang <mahikgot@gmail.com>
2025-12-15 23:13:18 +0800

    change pc name to markbook

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.pkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   # Work-in-progress: refer to parent/sibling flakes in the same repository
   outputs = all@{ self, pkgs, ... }: {
-    nixosConfigurations.nixos = pkgs.lib.nixosSystem {
+    nixosConfigurations.markbook = pkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [
         ./configuration.nix
